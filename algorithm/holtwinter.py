1fdac6d66db816fecad73dbca5c85df1e87daf4d
kien <trungkien2104@gmail.com.vn>
2023-03-29 16:39:55 +0700

    add algorithm

--- algorithm/holtwinter.py
+++ algorithm/holtwinter.py
@@ -1,5 +1,6 @@
 from . import BaseAlgorithm
 import pandas as pd
+import numpy as np
 from statsmodels.tsa.api import ExponentialSmoothing
 
 
@@ -12,7 +13,52 @@ class HoltWinter(BaseAlgorithm):
             config (dict): the tuning argument for the algorithm
             data (list): the data input from grafana. A list contains only 2 arrays: one time array and one values array
         """
-        super(TSDecomposition, self).__init__(config, data)
+        super(HoltWinter, self).__init__(config, data)
     
     def detect(self):
-        pass
+        """
+        Detect anomaly based on argument in "config" attribute
+        """
+        # Init input - Start
+        trend = "add"
+        seasonal = "add"
+        seasonal_periods = None
+        damped_trend = False
+        use_boxcox = False
+        threshold = 3
+        show_bound = False
+        if "trend" in self.config:
+            trend = self.config["trend"]
+        if "seasonal" in self.config:
+            seasonal = self.config["seasonal"]
+        if "seasonal_periods" in self.config:
+            seasonal_periods = self.config["seasonal_periods"]
+        if "damped_trend" in self.config:
+            damped_trend = self.config["damped_trend"]
+        if "use_boxcox" in self.config:
+            use_boxcox = self.config["use_boxcox"]
+        if "threshold" in self.config:
+            threshold = self.config["threshold"]
+        if "show_bound" in self.config:
+            show_bound = self.config["show_bound"]
+        # Init input - End
+        df = pd.DataFrame({'time': self.data[0], 'data': self.data[1]})
+        df['time'] = pd.to_datetime(df['time'], unit='ms')
+        df.set_index("time", inplace=True)
+        model = ExponentialSmoothing(df, seasonal_periods=seasonal_periods, trend=trend, seasonal=seasonal,
+                                    damped_trend=damped_trend, use_boxcox=use_boxcox).fit()
+        resid = model.resid
+        upper, lower = [resid.mean() + threshold * resid.std(), resid.mean() - threshold * resid.std()]
+        df['upper'] = model.fittedvalues + upper
+        df['lower'] = model.fittedvalues + lower
+        df.index = df.index.view(np.int64)//10**6
+        anomaly = df[(df['data'] < df['lower']) & (df['data'] > df['upper'])]['data']
+        return_value = {}
+        if len(anomaly) == 0:
+            return_value['anomaly'] = {}
+        else:
+            return_value['anomaly'] = anomaly.to_dict()
+        if show_bound:
+            return_value['upper'] = df['upper'].to_dict()
+            return_value['lower'] = df['lower'].to_dict()
+        return return_value
--- algorithm/tsdecomposition.py
+++ algorithm/tsdecomposition.py
@@ -1,4 +1,7 @@
 from . import BaseAlgorithm
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 
 class TSDecomposition(BaseAlgorithm):
@@ -13,4 +16,37 @@ class TSDecomposition(BaseAlgorithm):
         super(TSDecomposition, self).__init__(config, data)
     
     def detect(self):
-        pass
+        # Init input - Start
+        model = "additive"
+        period = 7
+        threshold = 3
+        show_bound = False
+        if "model" in self.config:
+            model = self.config['model']
+        if "period" in self.config:
+            period = self.config['period']
+        if "threshold" in self.config:
+            threshold = self.config["threshold"]
+        if "show_bound" in self.config:
+            show_bound = self.config["show_bound"]
+        # Init input - End
+        df = pd.DataFrame({'time': self.data[0], 'data': self.data[1]})
+        df['time'] = pd.to_datetime(df['time'], unit='ms')
+        df.set_index("time", inplace=True)
+        ds_result = seasonal_decompose(df, model=model, period=period)
+        resid = ds_result.resid
+        upper, lower = [resid.mean() + threshold * resid.std(), resid.mean() - threshold * resid.std()]
+        df['upper'] = ds_result.trend + ds_result.seasonal + upper
+        df['lower'] = ds_result.trend + ds_result.seasonal + lower
+        df.dropna(inplace=True)
+        df.index = df.index.view(np.int64)//10**6
+        anomaly = df[(df['data'] < df['lower']) & (df['data'] > df['upper'])]['data']
+        return_value = {}
+        if len(anomaly) == 0:
+            return_value['anomaly'] = {}
+        else:
+            return_value['anomaly'] = anomaly.to_dict()
+        if show_bound:
+            return_value['upper'] = df['upper'].to_dict()
+            return_value['lower'] = df['lower'].to_dict()
+        return return_value
